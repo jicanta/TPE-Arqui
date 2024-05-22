bb0980463d6f8c378069798b15bf901e87183daa
Josefina Gonzalez Cornet <117315719+jgonzalezcornet@users.noreply.github.com>
2024-05-22 14:26:04 -0300

    Update lib.h

--- barebones/Kernel/include/lib.h
+++ barebones/Kernel/include/lib.h
@@ -8,4 +8,18 @@ void * memcpy(void * destination, const void * source, uint64_t length);
 
 char *cpuVendor(char *result);
 
+char* itoa(int val, int base);
+
+uint32_t get_minutes();
+uint32_t get_seconds();
+uint32_t get_hours();
+
+void printMinutes(int x, int y);
+
+void printHours(int x, int y);
+
+void printSeconds(int x, int y);
+
+void printCurTime(int x, int y);
+
 #endif
