d20186d9a9bdac0241dac5b6c246c022e5da884b
Josefina Gonzalez Cornet <117315719+jgonzalezcornet@users.noreply.github.com>
2024-05-22 14:21:55 -0300

    interrupts.asm done

---
+++ barebones/Kernel/asm/interrupts.asm
@@ -0,0 +1,151 @@
+GLOBAL _cli
+GLOBAL _sti
+GLOBAL picMasterMask
+GLOBAL picSlaveMask
+GLOBAL haltcpu
+GLOBAL _hlt
+
+GLOBAL _irq02Handler
+GLOBAL _irq03Handler
+GLOBAL _irq04Handler
+GLOBAL _irq05Handler
+
+GLOBAL irq00                ; timer tick interruption
+GLOBAL irq01                ; keyboard interruptionn
+GLOBAL exception00          ; exception 00 (division by zero)
+GLOBAL exception06          ; exception 06 (invalid op code)
+
+EXTERN irqHandler           ; function in IRQs.c which handles interruption
+EXTERN exceptionHandler     ; function in exceptions.c which handles exceptions
+
+SECTION .text
+
+%macro pushState 0
+	push rax
+	push rbx
+	push rcx
+	push rdx
+	push rbp
+	push rdi
+	push rsi
+	push r8
+	push r9
+	push r10
+	push r11
+	push r12
+	push r13
+	push r14
+	push r15
+%endmacro
+
+%macro popState 0
+	pop r15
+	pop r14
+	pop r13
+	pop r12
+	pop r11
+	pop r10
+	pop r9
+	pop r8
+	pop rsi
+	pop rdi
+	pop rbp
+	pop rdx
+	pop rcx
+	pop rbx
+	pop rax
+%endmacro
+
+%macro irqHandlerMaster 1
+	pushState
+
+	mov rdi, %1 ; pasaje de parametro
+	call irqHandler                    ; calling irqHandler to process interruption
+
+	; signal pic EOI (End of Interrupt)
+	mov al, 20h
+	out 20h, al
+
+	popState
+	iretq
+%endmacro
+
+%macro exceptionHandler 1
+	pushState
+
+	mov rdi, %1 ; pasaje de parametro
+	call exceptionHandler             ; calling exceptionHandler to process interruption
+
+	popState
+	iretq
+%endmacro
+
+_hlt:
+	sti
+	hlt
+	ret
+
+_cli:
+	cli
+	ret
+
+_sti:
+	sti
+	ret
+
+picMasterMask:
+	push rbp
+    mov rbp, rsp
+    mov ax, di
+    out	21h,al
+    pop rbp
+    retn
+
+picSlaveMask:
+	push    rbp
+    mov     rbp, rsp
+    mov     ax, di  ; ax = mascara de 16 bits
+    out	0A1h,al
+    pop     rbp
+    retn
+
+; timer tick
+irq00:
+	irqHandlerMaster 0
+
+; keyboard
+irq01:
+	irqHandlerMaster 1
+
+;Cascade pic never called
+_irq02Handler:
+	irqHandlerMaster 2
+
+;Serial Port 2 and 4
+_irq03Handler:
+	irqHandlerMaster 3
+
+;Serial Port 1 and 3
+_irq04Handler:
+	irqHandlerMaster 4
+
+;USB
+_irq05Handler:
+	irqHandlerMaster 5
+
+; division by zero exception
+exception00:
+	exceptionHandler 0
+
+; invalid op code exception
+exception06:
+    exceptionHandler 1
+
+haltcpu:
+	cli
+	hlt
+	ret
+
+SECTION .bss
+	aux resq 1
+ 
