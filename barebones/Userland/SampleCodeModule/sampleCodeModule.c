ba8ee47c6a25aff495f9e74d9679b8332ceb9d46
jgonzalezcornet <jgonzalezcornet@itba.edu.ar>
2024-06-04 09:04:14 -0300

    added test 00

--- barebones/Userland/SampleCodeModule/sampleCodeModule.c
+++ barebones/Userland/SampleCodeModule/sampleCodeModule.c
@@ -1,5 +1,8 @@
 #include <shell.h>
 
+void test00(void);
+
 int main() {
+	//test00();
 	runShell();
 }
---
+++ barebones/Userland/SampleCodeModule/test00.asm
@@ -0,0 +1,25 @@
+GLOBAL test00
+section .text
+
+; ver foto de cuales estan bien y cuales mal, anda medio falopa
+test00:
+    mov rax, 01h
+    mov rbx, 02h
+    mov rcx, 03h
+    mov rdx, 04h
+    mov rsi, 05h
+    mov rdi, 06h
+    ; el rbp no lo quiero modificar
+    ; el rsp tampoco
+    mov r8, 08h
+    mov r9, 09h
+    mov r10, 10h
+    mov r11, 11h
+    mov r12, 12h
+    mov r13, 13h
+    mov r14, 14h
+    mov r15, 15h
+
+    ;div 0      ; TODO: el problema es que no me deja compilarlo si pongo div 0 lol como lo pruebo
+
+	ret
